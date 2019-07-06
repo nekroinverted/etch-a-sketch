be523b5148051763d2e1a5dc89b0c8dc4892a299
Marko <m-validzic@live.com>
2019-07-06 12:32:48 +0200

    Add dynamic div size

---
+++ script.js
@@ -0,0 +1,17 @@
+const container = document.querySelector("#container");
+let num = 16;
+let size = 100 / num;
+
+for(i = 0; i < num; i++){
+    for(j = 0; j < num; j++){
+        const divElement = document.createElement("div");
+      divElement.classList.add("containerelement");
+        divElement.style.backgroundColor = "#E0E0D1";
+        divElement.style.border = "black 1px solid";
+        divElement.style.width = `${size}%`;
+        divElement.style.height = `${size}%`;
+        container.appendChild(divElement);
+        console.log("div created");
+
+    }
+}
