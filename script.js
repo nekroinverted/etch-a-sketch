e2c64b748b66bc7715a0931ab086dabd325cb68e
Marko <m-validzic@live.com>
2019-07-07 13:37:09 +0200

    add reset button to create new grids

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 const container = document.querySelector("#container");
-let num = 16;
-let size = 100 / num;
 
-for(i = 0; i < num; i++){
-    for(j = 0; j < num; j++){
-        const divElement = document.createElement("div");
+createGrid(16);
+function createGrid(number) {
+  let size = 100 / number;
+  for (i = 0; i < number; i++) {
+    for (j = 0; j < number; j++) {
+      const divElement = document.createElement("div");
       divElement.classList.add("containerelement");
-        divElement.style.backgroundColor = "#E0E0D1";
-        divElement.style.border = "black 1px solid";
-        divElement.style.width = `${size}%`;
-        divElement.style.height = `${size}%`;
-        container.appendChild(divElement);
-        console.log("div created");
-
+      divElement.style.backgroundColor = "#E0E0D1";
+      divElement.style.border = "black 1px solid";
+      divElement.style.width = `${size}%`;
+      divElement.style.height = `${size}%`;
+      container.appendChild(divElement);
     }
+  }
+}
+function userPrompt() {
+  let numberOfDivs = prompt(
+    "Enter a number between 1 and 100 to create a grid:"
+  );
+  if (numberOfDivs < 1 || numberOfDivs > 100) {
+    alert("Error");
+  }
+  emptyGrid();
+  createGrid(numberOfDivs);
 }
 
-container.addEventListener("mouseover", function(e){
-    if(e.target.classList.contains("containerelement")){
-        e.target.style.backgroundColor = "black";
-    }
-})
+function emptyGrid() {
+  let range = document.createRange();
+  range.selectNodeContents(container);
+  range.deleteContents();
+}
+
+container.addEventListener("mouseover", function(e) {
+  if (e.target.classList.contains("containerelement")) {
+    e.target.style.backgroundColor = "black";
+  }
+});
